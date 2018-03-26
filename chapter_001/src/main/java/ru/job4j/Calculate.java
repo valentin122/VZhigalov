6ee33e07c7a8e16127a7264bfd30fd3584ba3d77
valentin122 <val1915@yandex.ru>
2018-03-26 22:09:02 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,16 @@
+/**
+*Calculate
+*
+*@author Valentin Zhigalov (VAL1915@ya.ru)
+*@since 23.03.18
+*@version 0.1
+*/
+
 package ru.job4j;
+
+/**
+*main - точка входа в программу
+*/
 
 public class Calculate {
 	public static void main (String[] args) {
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Calculate
+*
+*@author Valentin Zhigalov (VAL1915@ya.ru)
+*@since 23.03.18
+*@version 0.1
+*/
+
+package ru.job4j;
