bef83811b2f26b5d4b9a1787ff4aea7ebe25bc5a
valentin122 <val1915@yandex.ru>
2018-06-04 00:37:53 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -6,15 +6,26 @@
 *@version 0.1
 */
 
-package ru.job4j;
+package ru.job4j.Calculate;
 
-/**
-*main - точка входа в программу
-*/
+
 
 public class Calculate {
+	/**
+	*main - точка входа в программу
+	*@param args - args.
+	*/
 	public static void main (String[] args) {
 		System.out.println("Hello world");
 	}
 	
+	/**
+     * Method echo.
+     * @param name Valentin.
+     * @return Echo plus Valentin.
+     */
+    public String echo(String name) {
+		String input = "Valentin Zhigalov"
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,34 @@
+/**
+*CalculateTest
+*
+*@author Valentin Zhigalov (VAL1915@ya.ru)
+*@since 03.06.18
+*@version 0.2
+*/
+
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*/
+
+public class CalculateTest {
+	/**
+	*Test echo
+	*@Test
+	*/
+	public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Valentin Zhigalov";
+    String expect = "Echo, echo, echo : Valentin Zhigalov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+	}
+ 
+}
+	
+	
