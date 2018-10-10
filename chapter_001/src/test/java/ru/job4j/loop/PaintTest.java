e7bd75556de5f20f1c845c3cafc197cf6aa49d2c
valentin122 <val1915@yandex.ru>
2018-10-10 12:02:41 +0300

    5.4. Построить пирамиду в псевдографике.
                                                                       Выполнил форматирование кода по рекомендации ментора. Добавил отсутствующие методы для левой и правой пирамид.

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -1,20 +1,55 @@
+/**
+ * PaintTest
+ *
+ * @author Valentin Zhigalov (VAL1915@ya.ru)
+ * @version 0.1
+ * @since 08.10.18
+ */
+
 package ru.job4j.loop;
 
 import org.junit.Test;
+
 import java.util.StringJoiner;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-
-/**
- *PaintTest
- *
- *@author Valentin Zhigalov (VAL1915@ya.ru)
- *@since 08.10.18
- *@version 0.1
- */
-
 public class PaintTest {
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
     @Test
     public void whenPyramid4Height() {
         Paint paint = new Paint();
