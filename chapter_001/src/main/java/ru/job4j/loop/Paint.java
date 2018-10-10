9190ed75d2efe06444b5c66021beead335a0b93d
valentin122 <val1915@yandex.ru>
2018-10-10 12:29:45 +0300

    5.5. Рефакторинг кода. Выполнил форматирование кода по рекомендации ментора. Добавил отсутствующие методы для левой и правой пирамид.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,51 +1,30 @@
 package ru.job4j.loop;
+
 import java.util.function.BiPredicate;
+
 /**
- *Paint
+ * Paint
  *
- *@author Valentin Zhigalov (VAL1915@ya.ru)
- *@since 08.10.18
- *@version 0.1
+ * @author Valentin Zhigalov (VAL1915@ya.ru)
+ * @version 0.2
+ * @since 10.10.18
  */
 
 public class Paint {
     public String rightTrl(int height) {
-        // Буфер для результата.
-        StringBuilder screen = new StringBuilder();
-        // ширина будет равна высоте.
-        int weight = height;
-        // внешний цикл двигается по строкам.
-        for (int row = 0; row != height; row++) {
-            // внутренний цикл определяет положение ячейки в строке.
-            for (int column = 0; column != weight; column++) {
-                // если строка равна ячейки, то рисуем галку.
-                // в данном случае строка определяем, сколько галок будет на строке
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // добавляем перевод строки.
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
 
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= weight - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
     }
 
     public String pyramid(int height) {
