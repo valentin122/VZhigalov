9eada693c4969624eff8104a2fd52e7a92d03eb0
valentin122 <val1915@yandex.ru>
2018-10-10 11:48:46 +0300

    5.3. Построить шахматную доску в псевдографике.

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -1,9 +1,9 @@
 /**
- *Board
+ * Board
  *
- *@author Valentin Zhigalov (VAL1915@ya.ru)
- *@since 08.10.18
- *@version 0.1
+ * @author Valentin Zhigalov (VAL1915@ya.ru)
+ * @version 0.1
+ * @since 08.10.18
  */
 
 package ru.job4j.loop;
@@ -14,15 +14,14 @@ public class Board {
         String ln = System.lineSeparator();
         for (int h = 0; h < height; h++) {
             for (int w = 0; w < width; w++) {
-                // условие проверки, что писать пробел или X
-                // Выше в задании мы определили закономерность, когда нужно проставлять X
+
                 if ((h + w) % 2 == 0) {
                     screen.append("x");
                 } else {
                     screen.append(" ");
                 }
             }
-            // добавляем перевод на новую строку.
+
             screen.append(ln);
         }
         return screen.toString();
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -30,4 +30,13 @@ public class BoardTest {
     String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line);
     assertThat(result, is(expected));
    }
+
+    @Test
+    public void test() {
+        Board board = new Board();
+        String result = board.paint(4, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x %s x x%sx x %s x x%s", line, line, line, line);
+        assertThat(result, is(expected));
+    }
 }
