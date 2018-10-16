5207e4eac3d365083496673ddb2d245acf25cd27
valentin122 <val1915@yandex.ru>
2018-10-16 07:46:34 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,33 @@
+/**
+ * MatrixCheck
+ *
+ * @author Valentin Zhigalov (VAL1915@ya.ru)
+ * @version 0.1
+ * @since 16.10.18
+ */
+
+package ru.job4j.array;
+
+public class MatrixCheck {
+    /**
+     * Метод создания двумерного массива.
+     *
+     * @param data двумерный массив.
+     * @return все элементы по диагоналям равны true или false.
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length; i++) {
+            if (data[0][0] != data[i][i]) {
+                result = false;
+                break;
+            }
+
+            if (data[0][data.length - 1] != data[i][data.length - 1 - i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,39 @@
+/**
+ * MatrixCheckTest
+ *
+ * @author Valentin Zhigalov (VAL1915@ya.ru)
+ * @version 0.1
+ * @since 16.10.18
+ */
+
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true},
+                {false, true, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
