4e3b2ba0317e04c56d551d3f877e2c8b3e23129e
valentin122 <val1915@yandex.ru>
2018-10-16 07:45:49 +0300

    Небольшие косметические правки.

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -11,7 +11,7 @@ package ru.job4j.array;
 public class Check {
     public boolean mono(boolean[] data) {
         boolean result = true;
-        for (int index = 0; index < (data.length); index++) {
+        for (int index = 0; index < data.length; index++) {
 
             if (data[0] != data[index]) {
                 result = false;
