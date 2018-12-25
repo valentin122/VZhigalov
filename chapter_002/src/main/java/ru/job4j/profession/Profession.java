a526a1142df19a7eb6a5379999d618a09a725a18
valentin122 <val1915@yandex.ru>
2018-12-25 22:21:19 +0400

    Profession

---
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -0,0 +1,22 @@
+package ru.job4j.profession;
+
+/**
+ * Profession
+ *
+ * @author Valentin Zhigalov (VAL1915@ya.ru)
+ * @version 0.1
+ * @since 25.12.18
+ */
+
+public class Profession {
+    public String name;
+    public String profession;
+
+    public Profession(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
