f2329303dbc5878b1a977b6f5b87bd9963d400a6
valentin122 <val1915@yandex.ru>
2019-01-03 20:39:23 +0400

    Tracker v0.1

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker;
+
+/**
+ * Tracker
+ *
+ * @author Valentin Zhigalov (VAL1915@ya.ru)
+ * @version 0.1
+ * @since 02.01.19
+ */
+
+class Tracker {
+    private Item[] items = new Item[100];
+
+    public Item add(Item item){
+    }
+
+    public boolean replace(String id, Item item){
+    }
+
+    public boolean delete(String id){
+        return false;
+    }
+
+    public Item[] findAll(){
+    }
+
+    public Item[] findByName(String key){
+    }
+
+    public Item findById(String id){
+    }
+
+
+}
