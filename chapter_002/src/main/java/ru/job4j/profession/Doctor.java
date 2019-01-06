8386c03d0c321374fa846dcf9b621ebb1f454d64
valentin122 <val1915@yandex.ru>
2019-01-06 20:51:50 +0400

    Profession

--- chapter_002/src/main/java/ru/job4j/profession/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/profession/Doctor.java
@@ -2,11 +2,15 @@ package ru.job4j.profession;
 
 public class Doctor extends Profession{
 
+    public Doctor(String name) {
+        super(name);
+    }
+
     void treatPacient(Pacient pacient){
 
     }
 
     public Diagnose heal(Pacient pacient) {
-
+        return null;
     }
 }
--- chapter_002/src/main/java/ru/job4j/profession/Engineer.java
+++ chapter_002/src/main/java/ru/job4j/profession/Engineer.java
@@ -2,6 +2,11 @@ package ru.job4j.profession;
 
 public class Engineer extends Profession{
     String name;
+
+    public Engineer(String name) {
+        super(name);
+    }
+
     void build(Home home){
     }
 }
