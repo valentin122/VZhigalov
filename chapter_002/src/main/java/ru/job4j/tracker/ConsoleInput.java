5488f9ade5e2c5906dc546c6ac67ee863546328f
valentin122 <val1915@yandex.ru>
2019-03-18 21:07:03 +0400

    код на проверку.

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -8,7 +8,7 @@ public class ConsoleInput implements Input {
     @Override
     public String ask(String question) {
         System.out.println(question);
-        return scanner.next();
+        return scanner.nextLine();
     }
 
     @Override
--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -10,23 +10,23 @@ public class ValidateInput implements Input {
 
     @Override
     public String ask(String question) {
-        return String.valueOf(this.input.ask(question));
+        return this.input.ask(question);
     }
 
     @Override
     public int ask(String question, int[] range) {
-        boolean valid = true;
+        boolean valid = false;
         int value = -1;
         do {
             try {
                 value = this.input.ask(question, range);
-                valid = false;
+                valid = true;
             } catch (MenuOutException moe) {
                 System.out.println("Please select key from menu.");
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again.");
             }
-        } while (valid);
+        } while (!valid);
         return value;
     }
 }
