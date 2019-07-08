b6a7d0b15173c90232492c88052d13b5fe7d2ed8
valentin122 <val1915@yandex.ru>
2019-07-08 22:43:37 +0400

    3. Компаратор для строк. v4. Final!!! So I hope...

--- chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -6,21 +6,15 @@ import java.util.Comparator;
 public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result = 2;
+        int result = 0;
         String minWord = left.length() < right.length() ? left : right;
         for (int i = 0; i < minWord.length(); i++) {
-            result = Character.compare(left.charAt(i), right.charAt(i));
-            if (result != 0) {
+            int characterCompare = Character.compare(left.charAt(i), right.charAt(i));
+            if (characterCompare != 0) {
+                result = characterCompare;
                 break;
-            }
-            if (i == (minWord.length() - 1) && left.length() == right.length()) {
-                result = 0;
-                break;
-            }
-            if (i == (minWord.length() - 1)) {
-                result = minWord == left ? -1 : 1;
             }
         }
-        return result;
+        return result == 0 ? Integer.compare(left.length(), right.length()) : result;
     }
 }
