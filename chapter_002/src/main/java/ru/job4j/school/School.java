d853e7747d1ea38bc2d33714fb6561762d44a267
valentin122 <val1915@yandex.ru>
2019-09-15 11:43:02 +0400

    Stream API + checkstyle. 4. Преобразование List в Map.

--- chapter_002/src/main/java/ru/job4j/school/School.java
+++ chapter_002/src/main/java/ru/job4j/school/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.school;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,5 +11,13 @@ public class School {
         List<Student> result = students.stream().filter(predicate).collect(Collectors.toList());
         return result;
     }
-
+    public Map<String, Student> getMapOfListStudents(List<Student> students) {
+        Map<String, Student> result = students.stream().distinct().collect(
+                Collectors.toMap(
+                        Student -> Student.getLastname(),
+                        Student -> Student
+                )
+        );
+        return result;
+    }
 }
--- chapter_002/src/main/java/ru/job4j/school/Student.java
+++ chapter_002/src/main/java/ru/job4j/school/Student.java
@@ -2,6 +2,7 @@ package ru.job4j.school;
 
 public class Student {
     private int scope;
+    private String lastname;
 
     public Student(int scope) {
         this.scope = scope;
@@ -14,6 +15,11 @@ public class Student {
     public void setScope(int scope) {
         this.scope = scope;
     }
+
+    public String getLastname() {
+        return lastname;
+    }
+
     @Override
     public String toString() {
         return "scope is" + " " + scope;
