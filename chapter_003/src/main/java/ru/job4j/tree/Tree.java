e51aecd2e6478bf81239a2da909a72684ccb932a
valentin122 <val1915@yandex.ru>
2020-01-04 00:26:37 +0400

    Создать элементарную структуру дерева. Алгоритм поиска в ширину.

---
+++ chapter_003/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,84 @@
+package ru.job4j.tree;
+
+
+import java.util.*;
+
+
+public class Tree<E extends Comparable<E>> implements SimpleTree {
+    private int modCount = 0;
+    private int size = 0;
+
+    private Node<E> root;
+
+    public Tree(E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(Comparable parent, Comparable child) {
+        boolean rsl = false;
+        Optional<Node<E>> parentTemp = findBy(parent);
+        Optional<Node<E>> childTemp = findBy(parent);
+        if (parentTemp.isPresent()) {
+            if (childTemp.isPresent()) {
+                parentTemp.get().add(new Node<>(child));
+                modCount++;
+                size++;
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(Comparable value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue((E) value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public Iterator iterator() {
+        int iteratorModCount = modCount;
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.add(root);
+
+        return new Iterator() {
+
+            @Override
+            public boolean hasNext() {
+                return queue.size() > 0;
+            }
+
+            @Override
+            public E next() {
+                if (queue.size() == 0) {
+                    new NoSuchElementException();
+                }
+                if (iteratorModCount != modCount) {
+                    new ConcurrentModificationException();
+                }
+                Node<E> result = queue.poll();
+                for (Node<E> node : result.leaves()) {
+                    queue.add(node);
+                }
+                return result.getValue();
+            }
+        };
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/tree/TreeTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.tree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class TreeTest {
+    Tree<Integer> tree = new Tree<>(1);
+
+    @Before
+    public void setUp() {
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+    }
+    @Test
+    public void when6ElFindLastThen6() {
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void whenIterateTreeThenGetAllNodes() {
+        Iterator it = tree.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+
+    }
+
+}
