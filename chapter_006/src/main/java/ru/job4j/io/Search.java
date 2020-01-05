e663789f9928f252023d32f6447825e6d15ec7d9
valentin122 <val1915@yandex.ru>
2020-01-06 01:00:38 +0400

    Ввод-вывод. Сканирование файловой системы.

---
+++ chapter_006/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Search {
+
+    public List<File> files(String parent, List<String> exts) {
+        List<File> result = new ArrayList<>();
+        File file = new File(parent);
+        Queue<File> queue = new LinkedList<>();
+        for (File f : file.listFiles()) {
+            queue.add(f);
+        }
+        while (queue.size() > 0) {
+            File fileTemp = queue.poll();
+            if (fileTemp.isDirectory()) {
+                for (File f : fileTemp.listFiles()) {
+                    queue.add(f);
+                }
+            } else {
+                for (String ext : exts) {
+                    if (fileTemp.getName().contains(ext)) {
+                        result.add(fileTemp);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SearchTest {
+    String path = System.getProperty("java.io.tmpdir");
+    Search search = new Search();
+    File file;
+
+    @Before
+    public void setUp() throws Exception {
+        file = new File(path + "test789.txt");
+        FileWriter fileWriter = new FileWriter(file);
+    }
+
+    @After
+    public void tearDown() {
+         file.delete();
+    }
+
+    @Test
+    public void files() {
+        boolean actual = false;
+        List<String> ext = new ArrayList<>();
+        ext.add(".java");
+        ext.add(".txt");
+        List<File> result = search.files(path, ext);
+        for (File file : result) {
+            if (file.getName().equals("test789.txt")) {
+                actual = true;
+            }
+        }
+        assertThat(actual, is(true));
+    }
+}
