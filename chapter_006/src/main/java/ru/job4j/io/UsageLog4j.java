8d01b0249a1ab2dc94853b446d1c80281959a8f1
valentin122 <val1915@yandex.ru>
2021-06-14 15:05:39 +0300

    slf4j - шаблон для подстановки переменных.

--- chapter_006/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_006/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,24 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
+
+        boolean isMale = true;
+        char symbol = '\u0076';
+        byte ageByte =  20;
+        short ageShort = 21;
+        int ageInt = 22;
+        long ageLong = 23;
+        float ageFloat = 24.1f;
+        double ageDouble = 25.2;
+
+
+
         LOG.trace("trace message");
         LOG.debug("debug message");
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        LOG.debug("User info isMale: : {}, symbol : {}, ageByte : {}, ageShort : {}, ageInt : {}, ageLong : {},ageFloat : {}, ageDouble : {}",
+                isMale, symbol, ageByte, ageShort, ageInt, ageLong, ageFloat, ageDouble);
     }
 }
