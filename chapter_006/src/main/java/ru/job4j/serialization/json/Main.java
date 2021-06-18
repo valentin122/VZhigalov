1fa0f14565c63edeabfc5d4bf2663ba4851f8004
valentin122 <val1915@yandex.ru>
2021-06-19 00:17:03 +0300

    Сериализация.

--- chapter_006/src/main/java/ru/job4j/serialization/json/Main.java
+++ chapter_006/src/main/java/ru/job4j/serialization/json/Main.java
@@ -5,7 +5,7 @@ import com.google.gson.GsonBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        final Book book = new Book(false, 30, "Clean code", new Author("Роберт", "Мартин"), new String[]{"rev1", "rev2"});
+        final Book book = new Book(false, 464, "Clean code", new Author("Роберт", "Мартин"), new String[]{"rev1", "rev2"});
         final Gson gson = new GsonBuilder().create();
         String jsonString = gson.toJson(book);
         System.out.println(jsonString);
