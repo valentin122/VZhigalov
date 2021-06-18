41cc5fd31641f5e9dce1442586781390fcc57081
valentin122 <val1915@yandex.ru>
2021-06-19 01:18:34 +0300

    Сериализация. JAXB

--- chapter_006/src/main/java/ru/job4j/serialization/json/Main.java
+++ chapter_006/src/main/java/ru/job4j/serialization/json/Main.java
@@ -1,8 +1,5 @@
 package ru.job4j.serialization.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -11,13 +8,7 @@ import java.io.StringWriter;
 public class Main {
     public static void main(String[] args) throws JAXBException {
         final Book book = new Book(false, 464, "Clean code", new Author("Роберт", "Мартин"), new String[]{"rev1", "rev2"});
-     /*   final Gson gson = new GsonBuilder().create();
-        String jsonString = gson.toJson(book);
-    //    System.out.println(jsonString);
 
-        final Book book1 = gson.fromJson(jsonString, Book.class);
-    //    System.out.println(book1);
-*/
         JAXBContext context = JAXBContext.newInstance(Book.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
